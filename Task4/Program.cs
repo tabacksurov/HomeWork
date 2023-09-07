6d355b3573e63d5c73762e9c2e25823aba16578e
tabacksurov <tabacksurov@gmail.com>
2023-09-07 23:00:39 +0300

    Added code for task4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите последовательно три числа мы определим какое самое большое.");
+Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите третье число");
+int C = Convert.ToInt32(Console.ReadLine()!);
+int MAX = A;
+if (B > MAX) MAX = B;
+if (C > MAX) MAX = C;
+{
+    Console.WriteLine("Мы определили самое большое число которое Вы ввели это " + MAX);
+}
