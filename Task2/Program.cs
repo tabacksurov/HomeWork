befa9424775d410394e98bc4a2fac1611b279fd8
tabacksurov <tabacksurov@gmail.com>
2023-09-07 22:20:59 +0300

    Добавлено решение задачи на нахождение максимального значения из 2-х заданных чисел

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+Console.WriteLine("Введите два числа мы угадаем какое из ни больше");
+Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine()!);
+if (A >= B) 
+{
+    Console.WriteLine("Первое число "+ A + "больше чем число " + B);
+}
+else Console.WriteLine("Второе число " + B + "больше чем число " + A);
